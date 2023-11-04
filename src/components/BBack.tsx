e446d487886e77e506750df205113cc7e0f94f3f
kowalski <leejhwalker@gmail.com>
2023-11-04 09:51:48 +0100

    Changed header style and image method to match other sections and lists

--- src/components/BBack.tsx
+++ src/components/BBack.tsx
@@ -1,32 +1,32 @@
-// BackButton.tsx
+// BBack.tsx
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const BackButton: React.FC = () => {
+const BBack: React.FC = () => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-      <Text style={styles.text}>Back</Text>
-    </TouchableOpacity>
+    <View onLayout={(event) => {
+      const { width } = event.nativeEvent.layout;
+      // Assuming you want the button to take a certain percentage of the header's width
+      const imageSize = width * 0.15; // for example, 15% of the container's width
+      const imageStyle = {
+        width: imageSize,
+        height: imageSize, // keeping the image aspect ratio 1:1
+      };
+
+      return (
+        <TouchableOpacity onPress={() => navigation.goBack()} style={{ margin: 10 }}>
+          <Image
+            source={{ uri: 'https://placekitten.com/200/100' }}
+            style={imageStyle}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      );
+    }} />
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    margin: 10,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 4,
-  },
-  text: {
-    fontSize: 16,
-    color: '#000',
-  },
-});
-
-export default BackButton;
+export default BBack;
--- src/components/McentralHeaderSection.tsx
+++ src/components/McentralHeaderSection.tsx
@@ -1,31 +1,15 @@
 // McentralHeaderSection.tsx
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BackButton from './BBack';
+import { View, Text } from 'react-native';
+import BBack from './BBack';
 
-const McentralHeaderSection: React.FC<{ showBackButton?: boolean }> = ({ showBackButton }) => {
+const McentralHeaderSection: React.FC<{ showBBack?: boolean }> = ({ showBBack }) => {
   return (
-    <View style={styles.header}>
-      {showBackButton && <BackButton />}
-      <Text style={styles.headerText}>Header Section</Text>
+    <View style={{ height: 50, backgroundColor: '#FFA07A', flexDirection: 'row' }}>
+      {showBBack && <BBack />}
+      <Text>Header Section</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    height: 50, // Adjust the height as needed for your header space
-    justifyContent: 'center', // Changed to 'center' to align text properly
-    alignItems: 'center',
-    flexDirection: 'row',
-    paddingRight: 10, // Padding to keep the button inside the screen bounds
-    backgroundColor: '#FFA07A', // Salmon color for visibility
-  },
-  headerText: {
-    flex: 1, // Take up all available space
-    textAlign: 'center', // Center the text
-    color: 'black', // White color for the text for contrast
-  },
-});
 
 export default McentralHeaderSection;
