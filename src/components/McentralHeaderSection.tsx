562e9177a7268f443b7082aefac2519e7643666d
kowalski <leejhwalker@gmail.com>
2023-11-04 16:22:31 +0100

    The image shifts over the text in header so its there but not visible, damn I'm dumb

--- src/components/McentralHeaderSection.tsx
+++ src/components/McentralHeaderSection.tsx
@@ -1,12 +1,14 @@
 // McentralHeaderSection.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
-import BBack from './BBack';
+import { View, Text, Image } from 'react-native';
 
-const McentralHeaderSection: React.FC<{ showBBack?: boolean }> = ({ showBBack }) => {
+const McentralHeaderSection: React.FC = () => {
   return (
-    <View style={{ height: 50, backgroundColor: '#FFA07A', flexDirection: 'row' }}>
-      {showBBack && <BBack />}
+    <View style={{ height: 60, backgroundColor: 'lightsalmon', flexDirection: 'row' }}>
+      <Image
+        source={{ uri: 'https://placekitten.com/20/10' }}
+        style={{ width: 20, height: 10 }}
+      />
       <Text>Header Section</Text>
     </View>
   );
