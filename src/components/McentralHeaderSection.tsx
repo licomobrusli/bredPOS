f81cdc3eb76c9ab15688559838a431e4414f6f41
kowalski <leejhwalker@gmail.com>
2023-11-04 19:50:54 +0100

    Added branding image, adjusted back button image, made back button clickable (HeaderSection)

--- src/components/McentralHeaderSection.tsx
+++ src/components/McentralHeaderSection.tsx
@@ -1,15 +1,36 @@
 // McentralHeaderSection.tsx
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity, LayoutChangeEvent } from 'react-native';
 
 const McentralHeaderSection: React.FC = () => {
+  const [padding, setPadding] = useState<number>(0);
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+    setPadding(height / 12);
+  };
+
   return (
-    <View style={{ height: 60, backgroundColor: 'lightsalmon', flexDirection: 'row' }}>
+    <View
+      onLayout={onLayout}
+      style={{
+        height: 60,
+        backgroundColor: 'lightsalmon',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: padding,
+        alignItems: 'center'
+      }}>
       <Image
-        source={{ uri: 'https://placekitten.com/20/10' }}
-        style={{ width: 20, height: 10 }}
+        source={{ uri: 'https://placekitten.com/320/50' }}
+        style={{ width: 320, height: 50 }} // Adjust the size as needed
       />
-      <Text>Header Section</Text>
+      <TouchableOpacity style={{ alignSelf: 'flex-end' }}>
+        <Image
+          source={{ uri: 'https://placekitten.com/40/20' }}
+          style={{ width: 40, height: 20 }} // Adjust the size as needed
+        />
+      </TouchableOpacity>
     </View>
   );
 };
