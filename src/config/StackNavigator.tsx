3e56ca573ee695de9deab75994c693b89421fc00
kowalski <leejhwalker@gmail.com>
2023-11-05 20:04:35 +0100

    Updated StackNav to use CatScreen and SerScreen

--- src/config/StackNavigator.tsx
+++ src/config/StackNavigator.tsx
@@ -3,13 +3,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import CategoryList from '../content/CategoryList';
-import ServiceList from '../content/ServiceList';
+import CategoryScreen from '../screens/CategoryScreen';
+import ServiceScreen from '../screens/ServiceScreen';
 
 // If you are using TypeScript, you can define the types for your navigation parameters
 export type RootStackParamList = {
-  CategoryList: undefined;
-  ServiceList: { categoryCode: string };
+  CategoryScreen: undefined;
+  ServiceScreen: { categoryCode: string };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -18,13 +18,13 @@ const StackNavigator: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="ServiceList"
+        initialRouteName="CategoryScreen"
         screenOptions={{
         headerShown: false, // This will apply to all screens within this navigator
         }}
         >
-        <Stack.Screen name="CategoryList" component={CategoryList} />
-        <Stack.Screen name="ServiceList" component={ServiceList} />
+        <Stack.Screen name="CategoryScreen" component={CategoryScreen} />
+        <Stack.Screen name="ServiceScreen" component={ServiceScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
