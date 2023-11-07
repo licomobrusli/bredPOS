5e2ba6c22b214882dcc03c24d1b6d8e1bb4f36ee
kowalski <leejhwalker@gmail.com>
2023-11-07 06:54:30 +0100

    imported stackNav to MlistSection to display Category or Service dby NAv state.  Deleted NavContext as Mlist is a child of both Cat and Ser screens so can recieve NavStack directly: TESTED OK.  Next: Make CatScreen image onClick nav to SerScreen

--- src/components/MListSection.tsx
+++ src/components/MListSection.tsx
@@ -1,15 +1,19 @@
 // MlistSection.tsx
 import React from 'react';
 import { View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import ServiceList from '../content/ServiceList';
 import CategoryList from '../content/CategoryList';
 
 const MlistSection: React.FC = () => {
+  const route = useRoute();
+
   return (
     <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
-      <ServiceList />
+      {route.name === 'CategoryScreen' ? <CategoryList /> : <ServiceList />}
     </View>
   );
 };
+
 
 export default MlistSection;
