49bc8212dd9092ff98f9c17833bf1f99d3605a62
kowalski <leejhwalker@gmail.com>
2023-11-07 08:31:09 +0100

    Back only visible when stack >1. NEXT: Create modal for confirming select service and placing in the bottom section

--- src/content/BackButton.tsx
+++ src/content/BackButton.tsx
@@ -1,21 +1,28 @@
+// BackButton.tsx
 import React from 'react';
 import { TouchableOpacity, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
 
 const BackButton: React.FC = () => {
   const navigation = useNavigation();
+  const state = useNavigationState(state => state);
 
   const handlePress = () => {
     navigation.goBack();
   };
 
+  // Check if the current stack length is greater than 1
+  const isBackButtonVisible = state.routes.length > 1;
+
   return (
-    <TouchableOpacity onPress={handlePress}>
-      <Image
-        source={{ uri: 'https://placekitten.com/40/20' }}
-        style={{ width: 40, height: 20 }} // Adjust the size as needed
-      />
-    </TouchableOpacity>
+    isBackButtonVisible && (
+      <TouchableOpacity onPress={handlePress}>
+        <Image
+          source={{ uri: 'https://placekitten.com/40/20' }}
+          style={{ width: 40, height: 20 }} // Adjust the size as needed
+        />
+      </TouchableOpacity>
+    )
   );
 };
 
