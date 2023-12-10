eade16ef6393458d61ef9678e31fb907084cd259
kowalski <leejhwalker@gmail.com>
2023-12-10 19:58:05 +0100

    change header to show new mogans logo cut&go

--- src/components/MtopSection.tsx
+++ src/components/MtopSection.tsx
@@ -1,13 +1,18 @@
+// MTopSection.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
-import fonts from '../config/fonts';
+import { View, Image } from 'react-native';
+import { BANImage } from '../main/assets/images';
 import SDims from '../config/dimensions';
 
 const MtopSection: React.FC = () => {
   return (
-    <View style={{ height: SDims.HeightTopSection, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={fonts.txtBrandBanner}>MOGAN'S</Text>
-      <Text style={fonts.txtSubBrandBanner}>BARBER SHOP</Text>
+    <View style={{
+      height: SDims.HeightTopSection,
+      backgroundColor: 'black',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row' }}>
+      <Image source={BANImage} style={{ width: SDims.Width80p, height: '100%', resizeMode: 'contain' }} />
     </View>
   );
 };
