3d9df61de8b569c1f572d113b23704562f7fc99a
kowalski <leejhwalker@gmail.com>
2024-01-06 02:04:52 +0100

    services and categories both now use the context and providers rather than he api so I guess they should be fast now etc.  Last one to do is the ModalCounts.

--- src/content/CategoryList.tsx
+++ src/content/CategoryList.tsx
@@ -1,43 +1,21 @@
 // CategoryList.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { View, FlatList, Text } from 'react-native';
-import { fetchCategories } from '../config/apiCalls'; // Import your API calls
 import { Category } from '../config/types';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../config/StackNavigator';
 import { StackNavigationProp } from '@react-navigation/stack';
 import ListCard from '../components/ListCard';
 import { cardGridStyle } from '../config/cardGridStyle';
-
+import categoriesContext from '../config/categoriesContext'; // Make sure the path is correct
 
 const CategoryList = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const categories = useContext(categoriesContext); // Consuming the context
   const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'CategoryScreen'>>();
 
-  useEffect(() => {
-    const loadCategories = async () => {
-      setLoading(true);
-      try {
-        const loadedCategories = await fetchCategories(); // Fetch all categories
-        setCategories(loadedCategories); // Directly use the fetched categories
-        setError(null);
-      } catch (err) {
-        setError('Failed to fetch categories');
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    loadCategories();
-  }, []);
-  
   const onCategoryPress = (category: Category) => {
     navigation.navigate('ServiceScreen', { categoryCode: category.code, category: category });
-  };
-  
+  };  
 
   const renderItem = ({ item, index }: { item: Category | null; index: number }) => {
     const isPlaceholder = item === null;
@@ -69,7 +47,7 @@ const CategoryList = () => {
 
   return (
     <View style={{ flex: 1, backgroundColor: 'black', paddingBottom: cardGridStyle.margin}}>
-      {loading ? <Text>Loading...</Text> : error ? <Text>{error}</Text> : (
+      {!categories ? <Text>Loading...</Text> : (
         <FlatList
           data={categories}
           renderItem={renderItem}
