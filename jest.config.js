724e30f9576f87dc906ba924a0f0b8e965e93ef4
kowalski <leejhwalker@gmail.com>
2024-02-04 17:18:20 +0100

    a few adjustments to mock things for the jest like QR stuff it doesnt understand

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   preset: 'react-native',
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  }  
+  },  
+  transformIgnorePatterns: [
+    "node_modules/(?!(rn-qr-generator|react-navigation|react-native)/)"
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
 };
 
