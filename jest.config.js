435bdea2bae0253f8b20713f9207e57248ab13ef
kowalski <leejhwalker@gmail.com>
2024-02-04 17:35:29 +0100

    undoing some changes to babel.config.js and jest.config.js so that at least 1 of my 2 tests works again ... now take a break

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   preset: 'react-native',
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  },  
-  transformIgnorePatterns: [
-    "node_modules/(?!(rn-qr-generator|react-navigation|react-native)/)"
-  ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  }
+  }  
 };
-
